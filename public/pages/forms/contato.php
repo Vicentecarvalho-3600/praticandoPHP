1c1b59545edbd7fcfe811d1fbc6820024a31ef4f
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2023-12-07 02:52:14 -0300

    creating validation of form fields

--- public/pages/forms/contato.php
+++ public/pages/forms/contato.php
@@ -4,6 +4,13 @@ require '../../../bootstrap.php';
 
 
 
+if(isEmpty()){
+    flash('message', 'Preencha todos os campos',);
+
+    header('location: /php-quem-entende/public/?page=contato');
+
+}
+
 $validate = validate([
     'name' => 's',
     'email' => 'e',
